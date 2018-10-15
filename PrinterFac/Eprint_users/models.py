70012b54fe804fb39beca92e84192f151931c151
Anudeep Tubati <anudeep.tumati99@gmail.com>
2018-10-15 17:51:38 +0530

    v1.0

--- PrinterFac/Eprint_users/models.py
+++ PrinterFac/Eprint_users/models.py
@@ -10,14 +10,23 @@ def get_sentinel_user():  # Sets a user to deleted when a task of a delete user 
 # sqlmigrate app 000x for SQL code of migrate
 # for now, admin can control all tasks history
 # need to add price to each thing
-class PrintDocs(models.Model):
+class Bill(models.Model):
     ratePerPage = 3  # Rate per page in rupees, can add a model in AdminApp
     doc_name = models.CharField(max_length=500)
     pages = models.SmallIntegerField()
     copies = models.SmallIntegerField(default=1)
-    completed = models.BooleanField(default=False)
     date_added = models.DateTimeField(auto_now_add=True)
-    task_by = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
+    # completed = models.BooleanField(default=False)
+    # task_by = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
 
     def __str__(self):
         return self.doc_name + ', ' + str(self.date_added)
+
+
+class PrintDocs(models.Model):
+    fileToPrint = models.FileField()
+    docName = fileToPrint.name
+    colour = models.BooleanField()
+
+    def __str__(self):
+        return self.fileToPrint.name
--- PrinterFac/Eprint_users/views.py
+++ PrinterFac/Eprint_users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import PrintDocs
+from . models import Bill
 
 def history(request):
-    return render(request, 'Eprint_users/history.html', {'tasks': PrintDocs.objects.all()})
+    return render(request, 'Eprint_users/history.html', {'tasks': Bill.objects.all()})
